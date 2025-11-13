ee7190d70e2aa158142df6e00aa252987fe18581
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-13 16:08:16 +0600

    TASK-2.4 : add Bootstrap for improved styling and UI components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-serif;
+}
+
+#app {
+  min-height: 100vh;
+}
 </style>
