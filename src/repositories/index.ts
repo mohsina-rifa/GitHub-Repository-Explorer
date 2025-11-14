42d66589f48d031332f08cda70241fd2663204d0
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-14 19:06:30 +0600

    backlog : Remove unused repository files

--- src/repositories/index.ts
+++
@@ -1,5 +0,0 @@
-// Repository exports
-export { BaseRepository } from './base/baseRepository'
-export { GitHubRepository } from './gitHubRepository'
-export type { IRepository } from './interfaces/iRepository'
-export type { IGitHubRepository, GitHubRepositoryData } from './interfaces/iGitHubRepository'
