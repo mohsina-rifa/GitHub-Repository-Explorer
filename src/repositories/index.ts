7e6b20a7023c492acc1dcfc63e6801e212c38164
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-17 10:44:24 +0600

    Remove unused implementations

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,5 +1,3 @@
 // Repository exports
-export { BaseRepository } from './base/baseRepository'
-export { GitHubRepository } from './gitHubRepository'
 export type { IRepository } from './interfaces/iRepository'
 export type { IGitHubRepository, GitHubRepositoryData } from './interfaces/iGitHubRepository'
